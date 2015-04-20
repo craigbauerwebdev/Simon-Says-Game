7055f60cb3b9a5fc5b173c13ee81083ae521a77c
Craig Bauer <craig@craigbauerdesigns.com>
2015-04-20 06:57:00 -0400

    Added game over message

--- js/simonSays.js
+++ js/simonSays.js
@@ -8,46 +8,20 @@ angular.module('simonSaysApp', [])
 		}
 
 		gameOptionsDTO.prototype.dataObject = function() {
-			this.gameOptions = {
-				colorOptions: {
-					yellow: 'yellow', 
-					green: 'green', 
-					blue: 'blue', 
-					red: 'red',
-					},
-				random: ['yellow', 'green', 'blue', 'red'],
-				simonsTurn: false,
-				yourTurn: false,
-				simonMoves: [],
-				yourMoves: [],
-				displayMoves: [],
-
-			}
+			
 		};
 			
 			return gameOptionsDTO;
 		})
 
 
-	.service('allOptions', function(){
+	.service('turns', function(){
 
 	})
 
-	.value('colors', function(){
+	.value('gameOptionz', 
 
-	})
-
-	.controller('control', function($timeout, $interval, $log, gameOptionsDTO, allOptions, colors){
-
-		var self = this;
-
-		//self.go = new gameOptionsDTO;
-
-		self.isDisabled = true;
-
-
-// move values into this object slowly
-		self.gameOptions = {
+		{
 			colorOptions: {
 				yellow: 'yellow', 
 				green: 'green', 
@@ -62,42 +36,47 @@ angular.module('simonSaysApp', [])
 			displayMoves: [],
 
 		}
-// end object
 
-		self.colorOptions = ['yellow', 'green', 'blue', 'red'];
+	)
+
+	.controller('control', function($timeout, $interval, $log, gameOptionsDTO, gameOptionz, turns){
+
+		var self = this;
+
+		self.gO = gameOptionz;
+
+		self.isDisabled = true;
 
 		var i = 0;
 
 		self.yourTurn = function(colorSelect) {
-
 			
 			//run animation function on usr click
 			self.clickedColor = colorSelect;
 			self.selectSquare(self.clickedColor);
 
-			self.gameOptions.yourMoves.push({move: self.clickedColor});
+			self.gO.yourMoves.push({move: self.clickedColor});
 
 			self.len--;
 
-			self.yourMove = self.gameOptions.yourMoves[i].move;
-			self.simonsMove = self.gameOptions.simonMoves[i].move;
+			self.yourMove = self.gO.yourMoves[i].move;
+			self.simonsMove = self.gO.simonMoves[i].move;
 
 			if ( self.yourMove === self.simonsMove ) {
 
 				i++;
 
-				self.gameOptions.displayMoves.push({simon: self.yourMove, you: self.simonsMove});
+				self.gO.displayMoves.push({simon: self.yourMove, you: self.simonsMove});
 
 				// if your selections 
-				if ( self.gameOptions.yourMoves.length === self.gameOptions.simonMoves.length) {
-					
-					//$log.log('New Round');	
-					self.gameOptions.yourMoves = [];
+				if ( self.gO.yourMoves.length === self.gO.simonMoves.length) {
+						
+					self.gO.yourMoves = [];
 
 					self.isDisabled = true;
 
 					$timeout(function(){
-						self.gameOptions.displayMoves = [];
+						self.gO.displayMoves = [];
 						self.simonsTurn();
 					}, 1500);
 
@@ -105,17 +84,24 @@ angular.module('simonSaysApp', [])
 				}	
 
 			} else {
+					
+					self.gO.simonsTurn = false;
+					self.isDisabled = true;
+					self.gameOver = true;
 
-				i = 0;
-				self.gameOptions.yourMoves = [];
-				self.gameOptions.simonMoves = [];
-				self.gameOptions.displayMoves = [];
-				self.gameOptions.yourTurn = false;
-				self.gameOptions.simonsTurn = false;
-				self.isDisabled = true;
-				self.startBtn = false; //shows play again btn
 
-				console.log('Play Again?');
+				$timeout(function(){
+					i = 0;
+					self.isDisabled = true;
+					self.gO.yourMoves = [];
+					self.gO.simonMoves = [];
+					self.gO.displayMoves = [];
+					self.gO.yourTurn = false;
+					self.startBtn = false; //shows play again btn
+					self.gameOver = false;
+
+					console.log('Play Again?');
+				}, 2000);
 			}
 
 		}
@@ -123,41 +109,41 @@ angular.module('simonSaysApp', [])
 
 		self.simonsTurn = function(){
 
-			self.gameOptions.yourTurn = false;
-			self.gameOptions.simonsTurn = true;
+			self.gO.yourTurn = false;
+			self.gO.simonsTurn = true;
 
 
 			self.isDisabled = true; //shows button
 			self.startBtn = true; //hides btn
 
-			self.random = self.gameOptions.random[Math.floor(Math.random() * self.gameOptions.random.length)];
+			self.random = self.gO.random[Math.floor(Math.random() * self.gO.random.length)];
 
 			$log.info('Simon selected: ' + self.random);
 			
-			self.gameOptions.simonMoves.push({move: self.random});
+			self.gO.simonMoves.push({move: self.random});
 
-			self.len = self.gameOptions.simonMoves.length;
+			self.len = self.gO.simonMoves.length;
 
 			var x = 0; // Resets Every time simon takes a turn
 
 			$interval(function(){
 
 				// Runs through simons choices
-				if (self.gameOptions.simonMoves[x].move == 'yellow') {
+				if (self.gO.simonMoves[x].move == 'yellow') {
 
-					self.selectSquare(self.gameOptions.colorOptions.yellow);
+					self.selectSquare(self.gO.colorOptions.yellow);
 
-				} else if (self.gameOptions.simonMoves[x].move == 'green') {
+				} else if (self.gO.simonMoves[x].move == 'green') {
 
-					self.selectSquare(self.gameOptions.colorOptions.green);
+					self.selectSquare(self.gO.colorOptions.green);
 
-				} else if (self.gameOptions.simonMoves[x].move == 'blue') {
+				} else if (self.gO.simonMoves[x].move == 'blue') {
 
-					self.selectSquare(self.gameOptions.colorOptions.blue);
+					self.selectSquare(self.gO.colorOptions.blue);
 
-				} else if (self.gameOptions.simonMoves[x].move == 'red') {
+				} else if (self.gO.simonMoves[x].move == 'red') {
 
-					self.selectSquare(self.gameOptions.colorOptions.red);
+					self.selectSquare(self.gO.colorOptions.red);
 
 				} else {
 					$log.log('Oops... Simon Fucked Up!')
@@ -170,8 +156,8 @@ angular.module('simonSaysApp', [])
 				    }, 10);
 
 					$timeout(function(){
-				    	self.gameOptions.simonsTurn = false;
-						self.gameOptions.yourTurn = true;
+				    	self.gO.simonsTurn = false;
+						self.gO.yourTurn = true;
 					}, 1000);
 					
 				}
